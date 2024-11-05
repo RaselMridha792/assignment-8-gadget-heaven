078488d20d5cb8a8983ff5ac01c004fffda34533
rasel mridha <raselmridha792@gmail.com>
2024-11-05 18:18:56 +0600

    added category wise items and fixing the isActive button behaviour

--- src/component/Categories.jsx
+++ src/component/Categories.jsx
@@ -2,20 +2,33 @@ import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
 
 const Categories = ({ categories }) => {
+
+
   return (
     <>
-      <div role="tablist" className="tabs tabs-boxed flex flex-col">
-        <NavLink role='tab' className='tab' to='./'> All product</NavLink>
-        {
-            categories.map(category=> <NavLink role='tab' className='tab' key={category.category} to={`category/${category.category}`}>{category.category}</NavLink>)
-        }
+      <div role="tablist" className="tabs tabs-boxed flex flex-col bg-white">
+        <NavLink
+          className={({isActive})=>`${isActive?'bg-purple-600 text-white':''} text-xl w-full py-3 px-5 rounded-3xl`}
+          to="./"
+        >
+          All product
+        </NavLink>
+        {categories.map((category) => (
+          <NavLink
+            className={({isActive})=>`text-xl w-full ${isActive?'bg-purple-600 text-white':''} py-3 px-5 rounded-3xl`}
+            key={category.category}
+            to={`category/${category.category}`}
+          >
+            {category.category}
+          </NavLink>
+        ))}
       </div>
     </>
   );
 };
 
 Categories.propTypes = {
-    categories: PropTypes.array,
-}
+  categories: PropTypes.array,
+};
 
 export default Categories;
